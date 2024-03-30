dc713b5b2b1b31b5b3ab9a0f8307bbc96a50e909
JASONXU <xujason419@outlook.com>
2024-03-29 22:50:31 -0300

    feat: add login auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,51 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 
-
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
+  const passcode = req.body.password;
+  const currentLoginedUser = req.session.currentUser;
   //   const password = req.body.password;
+  if (!passcode) {
+    res.status(400).json({ message: "password is required" });
+    // throw new Error("password is required");
+  }
+  if (currentLoginedUser) {
+    res.status(403).json({ message: "A user already logged in!" });
+    // throw new Error("A user already logged in!");
+  }
   User.findAll({ where: { email: email } })
     .then((user) => {
       if (!user[0]) {
-        return res.json({ message: "User not found!" });
+        res.status(404).json({ message: "User not found!" });
+        throw new Error("User not found!");
       }
       return user[0];
     })
-    .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.currentUser = user;
-      return res.json({ message: "Logged in!", user: user });
+    .then((selectedUser) => {
+      bcrypt
+        .compare(passcode, selectedUser.password)
+        .then((result) => {
+          if (!result) {
+            return res.json({ message: "Invalid Credentials!" });
+          }
+          req.session.isLoggedIn = true;
+          req.session.currentUser = selectedUser;
+          return res.json({
+            message: "Logged in!",
+            user: {
+              userName: selectedUser.userName,
+              email: selectedUser.email,
+            },
+          });
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => console.log(err));
-  // todo: add the validation
-  //   res.setHeader("Set-Cookie", "loggedIn=true; Max-Age=3600; httpOnly");
-
-  //   req.session.isLoggedIn = true;
-  // todo: in the future, redirect will happen in react router
-
-  // res.json({message: "Logged in!"});
-  //   res.send("<h1>Logged in!</h1>");
 };
 exports.postLogOut = (req, res, next) => {
   req.session.destroy(() => {
-    res.json({ message: "session expired!" });
+    res.json({ message: "session destory!" });
   });
 };
 
@@ -42,12 +58,12 @@ exports.postSignUp = (req, res, next) => {
   User.findAll({ where: { email: email } })
     .then((user) => {
       if (user.length > 0) {
-        res.json({ message: "User already exist!" });
+        res.json({ message: "User already exist!" }).status(401);
         throw new Error("User already exist!");
       }
       return bcrypt.hash(password, 12);
     })
-    .then((hashedPassword)=>{
+    .then((hashedPassword) => {
       return User.create({
         email: email,
         password: hashedPassword,
@@ -60,7 +76,10 @@ exports.postSignUp = (req, res, next) => {
       req.session.currentUser = user;
       //create a roster
       user.createRoster();
-      res.json({ message: "User created!", user: {email:user.email,userName:user.userName} });
+      res.json({
+        message: "User created!",
+        user: { email: user.email, userName: user.userName },
+      });
     })
     .catch((err) => console.log(err));
 };
--- models/user.js
+++ models/user.js
@@ -21,6 +21,10 @@ const user = database.define("user", {
     type: Sequelize.STRING,
     allowNull: false,
   },
+  role: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
 });
 
 module.exports = user;
